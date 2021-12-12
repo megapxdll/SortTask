bc0c5e95d74d75041bab194f1b6ce677d1b3f6c6
Madi <mkuanyshbek97@mail.ru>
2021-12-12 17:20:07 +0600

    Second commit: Added Sorting algorithm by BubbleSort

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -14,11 +14,36 @@ public class main {
         for (int i = 0; i < allNotebook.length; i++) {
             Notebook notebook = new Notebook(random.nextInt(2000 - 50) + 50, random.nextInt(24 - 4) + 4, brandNames[random.nextInt(4)]);
             allNotebook[i] = notebook;
-            System.out.println(allNotebook[i].getBrandName()  + ", " + allNotebook[i].getOpMemory() + ", " + allNotebook[i].getCost());
         }
 
+        //Sorting by BubbleSort Algorithm
+        Notebook temp;
+        String tempBrandName = "";
+        for (int i = 0; i < allNotebook.length; i++) {
+            for (int j = 1; j < allNotebook.length - 1; j++) {
+                if(allNotebook[j-1].getCost() > allNotebook[j].getCost()) {
+                    temp = allNotebook[j-1];
+                    allNotebook[j-1] = allNotebook[j];
+                    allNotebook[j] = temp;
+                } else if (allNotebook[j -1].getCost() == allNotebook[j].getCost()) {
+                    if(allNotebook[j-1].getOpMemory() > allNotebook[j].getOpMemory()) {
+                        temp = allNotebook[j-1];
+                        allNotebook[j-1] = allNotebook[j];
+                        allNotebook[j] = temp;
+                    } else if (allNotebook[j-1].getOpMemory() == allNotebook[j].getOpMemory()) {
+                        temp = allNotebook[j-1];
+                        if(temp.getBrandName().charAt(0) > allNotebook[j].getBrandName().charAt(0)) {
+                            allNotebook[j-1] = allNotebook[j];
+                            allNotebook[j] = temp;
+                        }
+                    }
+                }
+            }
+        }
 
-        //Sort Algorithm
+        for (int i = 0; i < allNotebook.length; i++) {
+            System.out.println(allNotebook[i].getBrandName()  + ", " + allNotebook[i].getOpMemory() + ", " + allNotebook[i].getCost());
+        }
 
     }
 }
