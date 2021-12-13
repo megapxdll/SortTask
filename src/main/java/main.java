29c1866244b9913b1b152e4c8a4973c0cd836eaa
megapxdll <mkuanyshbek97@mail.ru>
2021-12-13 17:36:40 +0600

    Fifth commit: Added cost steps to 50 and opMemory steps to 4

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -8,11 +8,11 @@ public class main {
         //Initializing notebooks and other data like random generator and brand names
         Notebook [] allNotebook = new Notebook[10000];
         Random random = new Random();
-        String [] brandNames = new String[] {"Lenuvo ", "Asos ", "MacNote ", "Eser ", "Xamiou"};
+        String [] brandNames = new String[] {"Lenuvo", "Asos", "MacNote", "Eser ", "Xamiou"};
 
         //Filling array of notebooks with random values
         for (int i = 0; i < allNotebook.length; i++) {
-            Notebook notebook = new Notebook(random.nextInt(2000 - 50) + 50, random.nextInt(24 - 4) + 4, brandNames[random.nextInt(4)]);
+            Notebook notebook = new Notebook((random.nextInt(41 - 10) + 10) * 50, (random.nextInt(7 - 1) + 1) * 4, brandNames[random.nextInt(4)]);
             allNotebook[i] = notebook;
         }
 
