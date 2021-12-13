4c910470d5610065700e22269986e91e1152f091
megapxdll <mkuanyshbek97@mail.ru>
2021-12-13 14:47:20 +0600

    Third commit: Rebuilded soring algorithm, added optimization by searching for maximum value

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String[] args) {
 
         //Initializing notebooks and other data like random generator and brand names
-        Notebook [] allNotebook = new Notebook[10000];
+        Notebook [] allNotebook = new Notebook[100];
         Random random = new Random();
         String [] brandNames = new String[] {"Lenuvo ", "Asos ", "MacNote ", "Eser ", "Xamiou"};
 
@@ -16,11 +16,15 @@ public class main {
             allNotebook[i] = notebook;
         }
 
-        //Sorting by BubbleSort Algorithm
+        int pos = 0;
+        Notebook min;
+        Notebook max;
         Notebook temp;
         String tempBrandName = "";
-        for (int i = 0; i < allNotebook.length; i++) {
-            for (int j = 1; j < allNotebook.length - 1; j++) {
+        /**
+         //Sorting Algorithm
+        for (int i = 0; i < allNotebook.length; i ++) {
+            for (int j = 1; j < allNotebook.length - 1; j+=50) {
                 if(allNotebook[j-1].getCost() > allNotebook[j].getCost()) {
                     temp = allNotebook[j-1];
                     allNotebook[j-1] = allNotebook[j];
@@ -39,6 +43,32 @@ public class main {
                     }
                 }
             }
+        }
+         */
+
+        //Rebuild Sorting Algorithm
+        for (int i = allNotebook.length - 1; i >= 0; i--) {
+            pos = i;
+            //min = allNotebook[i];
+            max = allNotebook[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (allNotebook[j].getCost() > max.getCost()) {
+                    pos = j;
+                    max = allNotebook[j];
+                } else if (allNotebook[j].getCost() == max.getCost()) {
+                    if (allNotebook[j].getOpMemory() > max.getOpMemory()) {
+                        pos = j;
+                        max = allNotebook[j];
+                    } else if (allNotebook[j].getOpMemory() == max.getOpMemory()) {
+                        if (allNotebook[j].getBrandName().charAt(0) > max.getBrandName().charAt(0)) {
+                            pos = j;
+                            max = allNotebook[j];
+                        }
+                    }
+                }
+            }
+            allNotebook[pos] = allNotebook[i];
+            allNotebook[i] = max;
         }
 
         for (int i = 0; i < allNotebook.length; i++) {
